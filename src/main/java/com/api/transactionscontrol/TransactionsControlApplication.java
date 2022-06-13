38d4951474c3a29767daaa1a3917beb757e1d6b1
Camila de Farias Crispim <camila.dfc@hotmail.com>
2022-06-13 11:30:28 -0300

    initial configurantions

---
+++ src/main/java/com/api/transactionscontrol/TransactionsControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.transactionscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsControlApplication.class, args);
+	}
+
+}
