7147cfa4aaf90488046199143602445cfd7e5da9
Camila de Farias Crispim <camila.dfc@hotmail.com>
2022-06-19 23:30:47 -0300

    validate requests

--- src/main/java/com/api/transactionscontrol/account/AccountRequest.java
+++ src/main/java/com/api/transactionscontrol/account/AccountRequest.java
@@ -2,10 +2,12 @@ package com.api.transactionscontrol.account;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class AccountRequest {
   @NotNull(message = "Document number must be not null.")
-  @Pattern(regexp = "[0-9]*$", message = "Document number must be a number.")
+  @Pattern(regexp = "[0-9]*$", message = "Document number must be an integer.")
+  @Size(min=1, max=15, message = "Document number must be between 1 and 15 characters.")
   private String documentNumber;
 
   public String getDocumentNumber() {
